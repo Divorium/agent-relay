a0cf06cb189d713e6b9c6a2764fef06caaf392a4
mateusz-gorny <github.com@divo.net.pl>
2026-07-13 11:53:11 +0200

    Add error contracts

---
+++ src/contracts/errors.ts
@@ -0,0 +1,24 @@
+export type ErrorCode =
+  | "UNAUTHORIZED"
+  | "INVALID_REQUEST"
+  | "WORKSPACE_OUTSIDE_ROOT"
+  | "WORKSPACE_NOT_FOUND"
+  | "JOB_ALREADY_RUNNING"
+  | "REQUEST_ID_CONFLICT"
+  | "JOB_NOT_FOUND"
+  | "CODEX_FAILED"
+  | "CODEX_TIMEOUT"
+  | "RESULT_MISSING"
+  | "RESULT_INVALID"
+  | "INTERNAL_ERROR";
+
+export class RelayError extends Error {
+  constructor(
+    public readonly code: ErrorCode,
+    message: string,
+    public readonly statusCode: number,
+  ) {
+    super(message);
+    this.name = "RelayError";
+  }
+}
